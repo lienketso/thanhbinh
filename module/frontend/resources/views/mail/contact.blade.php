200e1c99d587123f8e8291e60165df50a13e3cc9
lienketso <thanhan1507@gmail.com>
2021-08-27 12:54:17 +0700

    send mail

--- module/frontend/resources/views/mail/contact.blade.php
+++ module/frontend/resources/views/mail/contact.blade.php
@@ -1,1 +1,11 @@
-<?php
+<!doctype html>
+<html lang="en">
+<meta charset="UTF-8">
+<body>
+<p>Bạn nhận được một tin nhắn liên hệ từ website www.thanhbinh-bca.vn</p>
+<p>Họ tên : <strong>{{$name}}</strong></p>
+<p>Email : <strong>{{$email}}</strong></p>
+<p>Tiêu đề : <strong>{{$title}}</strong></p>
+<p>Tin nhắn : <strong>{{$messenger}}</strong></p>
+</body>
+</html>
